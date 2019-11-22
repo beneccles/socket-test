05c10c4f7703925458a01fb4b4ce2d7ca68be399
Ben Eccles <ben.g.eccles@icloud.com>
2019-11-21 19:21:24 -0700

    added dependancies for server

---
+++ server/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const express = require('express')
+const socket = require('socket.io')
+const session = require('express-session')
+const cors = require('cors')
+const bodyParser = require('body-parser')
